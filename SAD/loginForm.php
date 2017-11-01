669bb5735a0829f7f1bf698a1ef6bbce26609c8a
Annah Jane Loyola <anajinkyut@gmail.com>
2017-11-01 20:32:52 +0800

    login form

---
+++ SAD/loginForm.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+
+	<form action="verify.php" method="POST">
+		
+	<input type="text" name="username" id="username" placeholder="Enter your username" /> <br />
+    <input type="password" name="password" id="password" placeholder="Enter your password" /> <br/>
+    <input type="submit" name="btnLogin" id="btn" value="Login" />
+
+	</form>
+
+</body>
+</html>
