a991d31f8c688f1efa3d68141a37c1ced2a2dcd2
Mina Bernal <csbernal2015@gmail.com>
2017-11-05 02:00:35 +0800

    invalid input
    put the "invalid input"

--- dashboard.php
+++ dashboard.php
@@ -4,18 +4,44 @@
 </head>
 <body>
 <?php
+    include 'connect.php';
+
 if(isset($_POST['btnLogin'])){
     $user = $_POST['user'];
     $pass = $_POST['pass'];
 
+      $sql = "SELECT * FROM account WHERE username = '$user' && password = '$pass'";
+
+       $result = $con->query($sql);
+
+       //clsoe the connection -->  
+        $con->close();
+
+
+    //check kung naa ba sa sulod sa dtabase ang gi input nga user ug pass
+    if($result->num_rows > 0) {
+        //if naa sa sulod execute ang dashboard nga menu
+?> 
+        <form  action="searchResult.php" method="post"  >
+            <input type="search" name="searchbar" id="search" placeholder="Enter text here" /> <br/>
+            <input type="submit" name="btnsearch" id="sbtn" value="GO" />
+    
+        </form>
+
+<?php
+      
+       
 }else{
+        // here dapat mag JScript nlng nga wala nag exist sa database ang user ug pass input
+     echo "invalid";
+    
+}
+    
+}else{
+    //kung wala gi input sa login nga mga user ug password nga mag prmpt cia sa login form
    header("location: login.php");
 }
 ?>
-<form  action="searchResult.php" method="post"  >
-	    <input type="search" name="searchbar" id="search" placeholder="Enter text here" /> <br/>
-        <input type="submit" name="btnsearch" id="sbtn" value="GO" />
-    
-</form>
+
 </body>
 </html>
