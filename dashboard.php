ed9a37a3524cd28a120284a16cd00b9f90acbad6
Mina Bernal <csbernal2015@gmail.com>
2017-11-05 01:00:56 +0800

    dbas,login,dashboard,resultspane
    connecting to the database,
    login form
    dashboard
    result pane after clicking the search button

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Dashbaord</title>
+</head>
+<body>
+
+<form  method="get" id ="search" action"searchResult.php">
+	<input type="text" name="search"  id="search" placeholder="Enter your here" /> <br />
+    <input name = "searchsubmit" type ="image" src="" value= "submit" id="searchButton" class="btn" /> <br/>
+    
+</form>
+</body>
+</html>
---
+++ searchResult.php
@@ -0,0 +1,6 @@
+<?php
+
+//here all the codes for query,
+//diri i pang gawas ang mga result sa search sa dashbard
+
+?>
