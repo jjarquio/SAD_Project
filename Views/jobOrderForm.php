de9aaf09918ae87805f104a3d70e88ca8a435cc8
iAnJe <anajinkyut@gmail.com>
2017-11-14 10:18:14 +0800

    job order form

--- Views/jobOrderForm.php
+++ Views/jobOrderForm.php
@@ -8,102 +8,102 @@
 
             <div>
 			<label>Job Order No.</label>
-			<input type="text" name="joNum" placeholder="Enter Username" required>
+			<input type="text" name="joNum" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Customer Name</label>
-			<input type="text" name="custName" placeholder="Enter Username" required>
+			<input type="text" name="custName" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Contact No.</label>
-			<input type="text" name="cnContact" placeholder="Enter Username" required>
+			<input type="text" name="cnContact" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Customer Address</label>
-			<input type="text" name="cnContact" placeholder="Enter Username" required>
+			<input type="text" name="cnContact" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Date Recieved</label>
-			<input type="text" name="date" placeholder="Enter Username" required>
+			<input type="text" name="date" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Item Code</label>
-			<input type="text" name="itmCode" placeholder="Enter Username" required>
+			<input type="text" name="itmCode" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Item / Product</label>
-			<input type="text" name="itmName" placeholder="Enter Username" required>
+			<input type="text" name="itmName" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Brand</label>
-			<input type="text" name="itmBrand" placeholder="Enter Username" required>
+			<input type="text" name="itmBrand" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Model</label>
-			<input type="text" name="itmModel" placeholder="Enter Username" required>
+			<input type="text" name="itmModel" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Serial No.</label>
-			<input type="text" name="sNum" placeholder="Enter Username" required>
+			<input type="text" name="sNum" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Quantity</label>
-			<input type="text" name="itemQty" placeholder="Enter Username" required>
+			<input type="text" name="itemQty" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Date</label>
-			<input type="text" name="datePur" placeholder="Enter Username" required>
+			<input type="text" name="datePur" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Accesories</label>
-			<input type="text" name="accesories" placeholder="Enter Username" required>
+			<input type="text" name="accesories" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Problem</label>
-			<input type="text" name="problem" placeholder="Enter Username" required>
+			<input type="text" name="problem" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Remarks</label>
-			<input type="text" name="remarks" placeholder="Enter Username" required>
+			<input type="text" name="remarks" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Service By</label>
-			<input type="text" name="servBy" placeholder="Enter Username" required>
+			<input type="text" name="servBy" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Address</label>
-			<input type="text" name="address" placeholder="Enter Username" required>
+			<input type="text" name="address" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Contact No.</label>
-			<input type="text" name="servContact" placeholder="Enter Username" required>
+			<input type="text" name="servContact" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Waybill</label>
-			<input type="text" name="wayBill" placeholder="Enter Username" required>
+			<input type="text" name="wayBill" placeholder="" required>
 			</div>
 
 			<div>
 			<label>Status</label>
-			<input type="text" name="itmStatus" placeholder="Enter Username" required>
+			<input type="text" name="itmStatus" placeholder="" required>
 			<select>
 				<option value="inOffice">In Office</option>
 				<option value="withSupplier">With Supplier</option>
