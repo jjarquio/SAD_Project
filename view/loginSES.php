5033a1a74999373a67aa9ae05f7e0a8f7de4ae1c
Annah Jane Loyola <anajinkyut@gmail.com>
2017-11-29 15:22:35 +0800

    login with session

--- view/loginSES.php
+++ view/loginSES.php
@@ -15,5 +15,39 @@
 </head>
 <body>
 
+	<form action="<?php $_PHP_SELF ?>" method="POST">
+		
+		<div>
+			Username: <input type="text" name="username" required/>
+			Password: <input type="password" name="password" minlength="8" required/>
+			<input type="submit" name="submitLOGIN" value="Login">
+		</div>
+
+	</form>
+
+	<?php
+
+		$username = isset($_POST['username'])?$_POST['username']:NULL;
+		$password = isset($_POST['password'])?$_POST['password']:NULL;
+
+		if (isset($_POST['submitLOGIN'])) {
+
+			include "../DBconnect/connection.php";
+
+			$sql="SELECT * FROM admin WHERE username = '$username' AND password = '$password'";
+			$result = $con->query($sql); 
+
+			if ($result->num_rows > 0){
+	       $_SESSION['username'] = $username;
+		   header("location: dashboard.php");	
+				}
+				else
+			echo "Account does not exist.";
+				$con->close();
+			
+		}
+
+	?>
+
 </body>
 </html>
