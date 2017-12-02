084c03e3951e88bdadba37f6842e055331a3c651
Mina Bernal <csbernal2015@gmail.com>
2017-12-03 00:11:01 +0800

    ANNAH
    ANNAH

--- old/searchResult.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['btnsearch'])){
-   
-    $itemSearched = $_POST['searchbar'];
-echo "bahug nawung!";
-}else{
-   header("location: login.php");
-}
-?>
--- view/generateReport.php
+++ view/generateReport.php
@@ -23,10 +23,14 @@
 	
 	
 
-		<a href="delivery_receipt.php">Delievery Receipt</a> <br>
-		<a href="job_order.php">Job Order Receipt</a> <br>
-		<a href="service_report.php">Service Report</a>
+		<li> <a href="job_order.php" target="frame" >Job Order Receipt</a></li>
+        <li> <a href="delivery_receipt.php" target="frame" >Delivery Receipt</a></li>
+    	<li> <a href="service_report.php" target="frame" > Service Report</a></li>
 
+  <section class="content">
+                        <iframe  width="50%" height="50%" name="frame" scolling="none" frameborder="2"</iframe>                
+
+	</section>
 
 
 </body>
