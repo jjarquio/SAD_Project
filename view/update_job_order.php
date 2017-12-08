9dad00a27034414552aec14b8f9a9fdf86cd5264
Annah Jane Loyola <anajinkyut@gmail.com>
2017-12-09 01:26:12 +0800

    if position is head mugawas ang edit else wala

--- view/update_job_order.php
+++ view/update_job_order.php
@@ -107,10 +107,20 @@
 						
                       
             
+		<?php
+
+		if ($_SESSION['POSITION'] == "head") {
+			
 		
 
-			<td><a  title="Click To Edit user" rel="facebox" href="edit_job_order.php?id=<?php echo $row['Job_order_no']; ?>"><button class="btn btn-warning btn-mini"><i class="icon-edit"></i> Edit </button></a> 
+		?>
+
+			<td><a  title="Click To Edit Job Order" rel="facebox" href="edit_job_order.php?id=<?php echo $row['Job_order_no']; ?>"><button class="btn btn-warning btn-mini"><i class="icon-edit"></i> Edit </button></a> 
             </tr>
+
+            <?php
+}
+            ?>
 
 	<?php	
                    }
