a30c05d3b72f6032e1412325bc3083a96aa5ba75
Annah Jane Loyola <anajinkyut@gmail.com>
2017-12-09 12:26:50 +0800

    i dunno pa sa scroll table so gi separate nlng nko by h1

--- view/notif.php
+++ view/notif.php
@@ -1,5 +1,6 @@
 <div><a href="index.php">Dashboard</a><br><br></div>
-
+<div>
+	<h1>Pending</h1>
 <?php
 
 $notif = 0;
@@ -14,12 +15,20 @@ session_start();
 	
 // if ang status kay pending tapos 7 days na and nilabay
 
-$sql = "SELECT Date_received, Job_order_no FROM joborderstatus WHERE Status = 'Pending' ";
+$sql = "SELECT * FROM joborderstatus WHERE Status = 'Pending' ";
 $result = $con->query($sql);
 if ($result) {
     while($row1 = $result->fetch_assoc()) {
     $res = $row1['Date_received'];
+    $itmNme = $row1['Item'];
+    $cNme = $row1['Customer_name'];
     $job = $row1['Job_order_no'];
+    $itmStat = $row1['Status'];
+
+    $datee = $row1['Date_received'];
+$dateee = strtotime($datee);
+$date = date('Y-m-d', $dateee);
+
 $exp_date = strtotime($res);
 $notify_start_date = strtotime('-7 days',$exp_date );
 $notify_end_date = strtotime('+15 days', $exp_date);
@@ -55,13 +64,21 @@ if ( $exp_date > $notify_start_date  &&  $exp_date < $notify_end_date ) {
 	<table>
 		<thead>
 			<tr>
+				<th>Date Created</th>
 				<th>Job Order No.</th>
+				<th>Customer Name</th>
+				<th>Item</th>
+				<th>Status</th>
 			</tr>
 		</thead>
 
 		<tbody>
 			<tr>
+				<td><?php echo $date; ?> </td>
 				<td><?php echo $job; ?> </td>
+				<td><?php echo $cNme; ?> </td>
+				<td><?php echo $itmNme; ?> </td>
+				<td><?php echo $itmStat; ?> </td>
 
 				<?php
 
@@ -83,8 +100,9 @@ if ( $exp_date > $notify_start_date  &&  $exp_date < $notify_end_date ) {
 
 </body>
 </html>
-
-
+</div>
+<div>
+	<h1>Work in Progress</h1>
 <?php
 /*echo 'nofity start date--->'.date('Y-m-d',$notify_start_date).'<br>';
 echo 'nofity end date--->'.date('Y-m-d',$notify_end_date).'<br>';
@@ -101,13 +119,20 @@ echo 'today--->'.date('Y-m-d',$now).'<br>';
 <?php
 // if ang status kay work in progress tapos 7 days na and nilabay
 
-$sql = "SELECT Edit_status_date, Job_order_no FROM joborderstatus WHERE Status = 'Work in Progress' ";
+$sql = "SELECT * FROM joborderstatus WHERE Status = 'Work in Progress' ";
 $result = $con->query($sql);
 
 if ($result) {
     while($row1 = $result->fetch_assoc()) {
     $res = $row1['Edit_status_date'];
+    $itmNme = $row1['Item'];
+    $cNme = $row1['Customer_name'];
     $job = $row1['Job_order_no'];
+    $itmStat = $row1['Status'];
+    $datee = $row1['Date_received'];
+$dateee = strtotime($datee);
+$date = date('Y-m-d', $dateee);
+
 $exp_date = strtotime($res);
 $notify_start_date = strtotime('-7 days',$exp_date );
 $notify_end_date = strtotime('+15 days', $exp_date);
@@ -115,7 +140,7 @@ $now = new DateTime();
 $now = $now->format('Y-m-d');
 $now = strtotime($now);
 
-echo $res;
+//echo $res;
 
 
 if ( $exp_date > $notify_start_date  &&  $exp_date < $notify_end_date ) {
@@ -141,14 +166,21 @@ if ( $exp_date > $notify_start_date  &&  $exp_date < $notify_end_date ) {
 	<table>
 		<thead>
 			<tr>
+				<th>Date Created</th>
 				<th>Job Order No.</th>
+				<th>Customer Name</th>
+				<th>Item</th>
+				<th>Status</th>
 			</tr>
 		</thead>
 
 		<tbody>
 			<tr>
+				<td><?php echo $date; ?> </td>
 				<td><?php echo $job; ?> </td>
-
+				<td><?php echo $cNme; ?> </td>
+				<td><?php echo $itmNme; ?> </td>
+				<td><?php echo $itmStat; ?> </td>
 				<?php
 
 		if ($_SESSION['POSITION'] == "head") {
@@ -183,6 +215,7 @@ echo 'today--->'.date('Y-m-d',$now).'<br>';
     }
 }
 ?>
+</div>
 <?php
 
  $_SESSION['NOTIF'] = $notif;
