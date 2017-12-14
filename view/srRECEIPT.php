ffb0b987fff8c11afa8c1f6536ffa432c6fee679
Annah Jane Loyola <anajinkyut@gmail.com>
2017-12-14 22:11:28 +0800

    service report RECEIPt to pdf

--- view/srRECEIPT.php
+++ view/srRECEIPT.php
@@ -1,5 +1,144 @@
 <?php
-		session_start(); 
+
+session_start(); 
+		include "../DBconnect/connection.php";
+      if(!isset($_SESSION['USERNAME']))
+	  {
+		  header("location:../index.php");
+	  }
+	  if (!$_GET['JobOrder']) {
+	header("location: generateReport.php");
+}
+	  $jobORDER = $_GET['JobOrder'];
+
+	  $sql = "SELECT * FROM joborderstatus WHERE Job_order_no = '$jobORDER'";
+	$result = $con->query($sql);
+	if($result){
+	$row=$result->fetch_assoc();
+	$datee = $row['Date_received'];
+    $dater = $row['Edit_status_date'];
+$dateee = strtotime($datee);
+$date = date('Y-m-d', $dateee);
+$daterr = strtotime($dater);
+$dateRET = date('Y-m-d', $daterr);
+}
+
+require("../fpdf/fpdf.php");
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont("Arial", "B", 18);
+$pdf->Cell(190,10," RASI CCOMPUTER INC. ",1,1,'C');
+
+
+$pdf->SetFont("Arial", "I", 9);
+$pdf->Text(64,23,"ROXAS AVE. NEAR PADRE GOMEZ ST., DAVAO CITY");
+$pdf->Text(64,23,"ROXAS AVE. NEAR PADRE GOMEZ ST., DAVAO CITY");
+$pdf->Text(68,27,"TEL# 222-2245/222-1075 - MOBILE: 09228457291");
+// box para sa location sa rasi
+
+$pdf->Line(10,20,10,28);
+$pdf->Line(200,20,200,28);
+$pdf->Line(200,28,10,28);
+
+$pdf->Line(10,28,10,30);
+$pdf->Line(200,28,200,30);
+$pdf->Line(200,30,10,30);
+$pdf->Ln(10);
+$pdf->SetFont("Arial", "", 15);
+$pdf->Cell(190,8,"SERVICE REPORT ",1,1,'C');
+$pdf->SetFont("Arial", "", 10);
+$pdf->Cell(76,5,"",1,0,'R');
+$pdf->Cell(32,5," DATE RECEIVED: ",1,0,'C');
+$pdf->Cell(25,5,$date,1,0,'C');
+
+$pdf->Cell(32,5," DATE RETURNED: ",1,0,'C');
+$pdf->Cell(25,5,$dateRET,1,1,'C');
+
+$pdf->Cell(60,5,"Job Order No.: ",1,0,'R');
+$pdf->Cell(130,5,$row['Job_order_no'],1,1,'L');
+
+$pdf->Cell(60,5," Client Name: ",1,0,'R');
+$pdf->Cell(130,5,$row['Customer_name'],1,1,'L');
+
+$pdf->Cell(60,5," Contact No.: ",1,0,'R');
+$pdf->Cell(130,5,$row['Contact_no'],1,1,'L');
+
+$pdf->Cell(60,5," Qty - Item/Brand/Model: ",1,0,'R');
+$pdf->Cell(130,5,$row['Quantity']." | ".$row['Item']." | ".$row['Brand']." | ".$row['Model'],1,1,'L');
+
+$pdf->Cell(60,5," Accessories: ",1,0,'R');
+$pdf->Cell(130,5,$row['Accessories'],1,1,'L');
+
+$pdf->SetFont("Arial", "U", 10);
+$pdf->Cell(60,5," Serial No.: ",1,0,'C');
+$pdf->Cell(130,5," Date of Purchase: ",1,1,'C');
+
+$pdf->SetFont("Arial", "", 10);
+$pdf->Cell(60,5,$row['Serial_no'],1,0,'C');
+$pdf->Cell(130,5,$row['Date_purchased'],1,1,'C');
+
+$pdf->SetFont("Arial", "U", 10);
+$pdf->Cell(190,5,"Problem",1,1,'C');
+
+$pdf->SetFont("Arial", "", 10);
+$pdf->Cell(190,5,$row['Problem'],1,1,'C');
+
+$pdf->Cell(60,5,"Replacement",1,0,'C');
+$pdf->Cell(130,5,"",1,1,'C');
+
+
+$pdf->Text(12,100,"TERMS AND CONDITION:");
+$pdf->Text(12,105,"1.");
+$pdf->SetFont("Arial", "I", 10);
+$pdf->Text(16,105,"RASI COMPUTER");
+$pdf->SetFont("Arial", "", 10);
+$pdf->Text(46,105,"is not liable for any DATA LOSS and UNLICENSED/PIRATED SOFTWARE that is presently");
+
+$pdf->Text(16,109,"in the computer brought in for check up and repair.");
+$pdf->Text(12,115,"2.");
+$pdf->Text(16,115,"Items not claimed after a period of three (3) months will automatically be come the property of RASI.");
+$pdf->Text(12,121,"3.");
+$pdf->Text(16,121,"A storage fee of P25/day will be charged if the item is not claimed within thirty(30) days after notice of completion.");
+$pdf->Text(12,127,"4.");
+$pdf->Text(16,127,"Job order must be kept and presented when claiming your item.");
+
+$pdf->Ln(40);
+$pdf->SetFont("Arial", "", 8);
+$pdf->Text(17,138,"RECEIVED THE ABOVE GOODS IN");
+$pdf->Text(18.5,141,"GOOD ORDER AND CONDITION");
+$pdf->Text(18.5,149,"____________________________");
+$pdf->Text(18,152,"PRINTED NAME AND SIGNATURE");
+$pdf->Text(100,141,"RELEASED BY:");
+$pdf->Text(88,149,"____________________________");
+$pdf->Text(87,152,"PRINTED NAME AND SIGNATURE");
+
+$pdf->Text(155,141,"DATE RELEASED:");
+$pdf->Text(153,149,"__________________");
+
+$pdf->Cell(60,20,"",1,0,'C');
+
+$pdf->Line(10,155,10,30);
+$pdf->Line(200,93,200,155);
+$pdf->Line(200,155,10,155);
+/*
+$pdf->Line(10,38,10,30);
+$pdf->Line(200,38,200,30);
+$pdf->Line(200,38,10,38);*/
+//$pdf->Cell(190,9," ROXAS AVE.NEAR PADRE GOMEZ ST., DAVAO CITY, ",1,1,'C');
+
+
+$pdf->Output('',"JobOrder ".$_GET['JobOrder'].".pdf");
+?>
+
+
+
+
+
+
+
+
+<!-- <?php 
+		/*session_start(); 
 		include "../DBconnect/connection.php";
       if(!isset($_SESSION['USERNAME']))
 	  {
@@ -201,4 +340,4 @@ $dateRET = date('Y-m-d', $daterr);
 }
 	?>
 </body>
-</html>
+</html>*/
