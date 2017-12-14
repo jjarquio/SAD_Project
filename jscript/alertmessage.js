70cf534d6892d835c1aa07dcba156a9dcdc2f7eb
Mina Bernal <csbernal2015@gmail.com>
2017-12-14 22:08:11 +0800

    remember me

--- jscript/alertmessage.js
+++
@@ -1,6 +0,0 @@
-myFunction();
-function added() {
-
-    alert("Account does not exist");
-   
-}
--- login.php
+++ login.php
@@ -32,34 +32,41 @@
 		$username = mysqli_real_escape_string($con, $_POST['username']);  //converting the string to cancell out all tags and  special chracters 
 		$password = mysqli_real_escape_string($con, $_POST['password']);  //converting the string to cancell out all the tagsa nd special characters
 	
-		$remember = $_POST['remember'];
+
 
 		//DB QUERY
 		$sql="SELECT * FROM admin  WHERE Username = '$username' AND password = '$password'";	 
 		$result = $con->query($sql); 
 		if ($result->num_rows > 0){
-	       while ($row = $result->fetch_array()) {
+	       if($row = $result->fetch_array()) {
 
 
 				$_SESSION['USERNAME']= $row[1];
 				$_SESSION['POSITION']=$row[3];
 				$_SESSION['NAME']=$row[0];
 
-				if($remember == "remember"){
-            setcookie("user[Username]",$row['username']);
-			setcookie("user[Password]",$row['password']);
-			}
+				if(!empty($_POST['remember'])){
+			
+					setcookie("username",$_POST['username'],time()+(10*365*24*60*60));
+					setcookie("password",$_POST['password'],time()+(10*365*24*60*60));
+				}else{
+					if(isset($_COOKIE['username'])){
+						setcookie("username","");
+					}
+					if(isset($_COOKIE['password'])){
+						setcookie("password","");
+					}
+				}
 
 				header ("location: view/index.php" );
 		   }
-		}
-		else{
+		}else{
 				
-				$_SESSION['ERROR_MESSAGE'] ='ACCOUNT_DONT_EXIST';		//NAG STORE UG ERROR MESSAGE SA SESSION, GAMITON AS FLAG SA LOGIN PARA MAG 
-				session_write_close();									//FUNCTION ANG JAVASCRIPT
+				$_SESSION['ERROR_MESSAGE'] ='ACCOUNT_DONT_EXIST';		
+											
 				header("location: index.php");
-				exit();
-		$con->close();
+			
+				$con->close();
 		}
 	}
 ?>
